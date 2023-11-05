1b385a02ffb027336ffc3187471c89701acfa967
Ashab Asir <113583194+Ashab-Asir@users.noreply.github.com>
2023-11-05 17:59:48 +0600

    Time: 88 ms (5.17%), Space: 66.1 MB (5.67%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    int totalDay(vector<int>weights,int mid){
-        int cntDay=1,ship=0;
-        for(int i=0;i<weights.size();i++){
-           if(weights[i]+ship>mid){
-               cntDay+=1;
-               ship=weights[i];
-           }
-           else{
-               ship+=weights[i];
-           }
+    int ispossible(vector<int>arr,int capacity){
+        int cntDay=1,load=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]+load<=capacity){
+                load+=arr[i];
+            }
+            else{
+                cntDay++;
+                load=arr[i];
+            }
         }
-        return cntDay;
+        return  cntDay;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        int low=INT_MIN;
+        int low=INT_MIN,high=0;
         for(int i=0;i<weights.size();i++){
-            low=max(weights[i],low);
-        }
-        int high=0;
-        int ans;
-        for(int i=0;i<weights.size();i++){
+            if(weights[i]>low){
+                low=weights[i];
+            }
             high+=weights[i];
         }
+        int ans;
         while(low<=high){
             int mid=(low+high)/2;
-            if(totalDay(weights,mid)<=days){
+            if(ispossible(weights,mid)<=days){
                 ans=mid;
                 high=mid-1;
             }
-            else{
+            else if(ispossible(weights,mid)>days){
                 low=mid+1;
             }
         }
