57225f589a20a3cebc2820ab2339e05eb234aa3b
Ashab Asir <113583194+Ashab-Asir@users.noreply.github.com>
2023-11-07 17:57:02 +0600

    Time: 0 ms (100%), Space: 7.4 MB (70.78%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,9 +3,12 @@ public:
     void rotate(vector<vector<int>>& matrix) {
        for(int i=0;i<matrix.size()-1;i++){
            for(int j=i+1;j<matrix.size();j++){
+               if(i==j){
+                   continue;
+               }
                swap(matrix[i][j],matrix[j][i]);
            }
-       } 
+       }
        for(int i=0;i<matrix.size();i++){
            reverse(matrix[i].begin(),matrix[i].end());
        }
