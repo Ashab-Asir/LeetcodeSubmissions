4ce96da7e99f2764f2dc78b64ed930a588686989
Ashab Asir <113583194+Ashab-Asir@users.noreply.github.com>
2024-05-30 09:01:59 +0600

    Time: 0 ms (100%), Space: 14.6 MB (53.77%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int index=-1;
-        int n=nums.size();
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                index=i;
-                break;
-            }
-        }
-        if(index==-1){
-            sort(nums.begin(),nums.end());
-        }
-        else{
-            for(int i=n-1;i>=index+1;i--){
-                if(nums[index]<nums[i]){
-                    swap(nums[index],nums[i]);
-                    break;
-                }
-            }
-            sort(nums.begin()+index+1,nums.end());
-        }
+        next_permutation(nums.begin(),nums.end());
     }
 };
