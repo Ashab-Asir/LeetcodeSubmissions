b0c9f2c826a7591aaf2b52efd6928f18c08e6cf0
Ashab Asir <113583194+Ashab-Asir@users.noreply.github.com>
2024-06-15 07:44:54 +0600

    Time: 142 ms (59.67%), Space: 85.3 MB (72.26%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -4,20 +4,21 @@ public:
          int n = profits.size();
          vector<pair<int,int>>projects;
         for (int i = 0; i < n; i++) {
-            projects.push_back({profits[i], capital[i]});
+            projects.push_back({capital[i], profits[i]});
         }
         sort(projects.begin(), projects.end());
         priority_queue<int> pq;
         int i = 0;
-        while (k--) {
-            while (i < n && projects[i].second <= w) {
-                pq.push(projects[i].first);
+        while (k) {
+            while (i < n && projects[i].first <= w) {
+                pq.push(projects[i].second);
                 i++;
             }
             if (!pq.empty()) {
                 w += pq.top();
                 pq.pop();
             }
+            k--;
         }
         return w;
     }
