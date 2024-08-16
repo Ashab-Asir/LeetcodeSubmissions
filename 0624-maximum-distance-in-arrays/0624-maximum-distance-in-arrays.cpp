0c34c63755d8e77a668fa8ac76a971d5efee8331
Ashab Asir <113583194+Ashab-Asir@users.noreply.github.com>
2024-08-16 10:47:36 +0600

    Time: 252 ms (22.17%), Space: 108 MB (29.72%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arrays) {
-        int mini = INT_MAX;
-        int maxi = INT_MIN;
+        int mini1 = INT_MAX,mini2=INT_MAX;
+        int maxi1 = INT_MIN,maxi2=INT_MIN;
         int minIdx, maxIdx;
         for (int i = 0; i < arrays.size(); i++) {
             for (int j = 0; j < arrays[i].size(); j++) {
-                if (arrays[i][j] > maxi) {
-                    maxi = arrays[i][j];
+                if (arrays[i][j] > maxi1) {
+                    maxi1 = arrays[i][j];
                     maxIdx = i;
                 }
             }
@@ -15,12 +15,32 @@ public:
         for (int i = 0; i < arrays.size(); i++) {
             if (maxIdx != i) {
                 for (int j = 0; j < arrays[i].size(); j++) {
-                    if (arrays[i][j] < mini) {
-                        mini = arrays[i][j];
+                    if (arrays[i][j] < mini1) {
+                        mini1 = arrays[i][j];
                     }
                 }
             }
         }
-        return maxi - mini;
+
+        for (int i = 0; i < arrays.size(); i++) {
+            for (int j = 0; j < arrays[i].size(); j++) {
+                if (arrays[i][j] < mini2) {
+                    mini2 = arrays[i][j];
+                    minIdx = i;
+                }
+            }
+        }
+        for (int i = 0; i < arrays.size(); i++) {
+            if (minIdx != i) {
+                for (int j = 0; j < arrays[i].size(); j++) {
+                    if (arrays[i][j] > maxi2) {
+                        maxi2 = arrays[i][j];
+                    }
+                }
+            }
+        }
+        int diff1=maxi1-mini1;
+        int diff2=maxi2-mini2;
+        return max(diff1,diff2);
     }
 };
