6d588e96f651aabb9986131f4b84b1145bcce5a7
Ashab Asir <113583194+Ashab-Asir@users.noreply.github.com>
2024-09-23 19:45:09 +0600

    Time: 819 ms (5.07%), Space: 294.9 MB (5.07%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
+    int t[52];
+    int solve(int i,string s,set<string>st,int n){
+        if(i>=n){
+            return 0;
+        }
+        if(t[i]!=-1){
+            return t[i];
+        }
+        int result=1+solve(i+1,s,st,n);
+        for(int j=i;j<n;j++){
+            string str=s.substr(i,j-i+1);
+            if(st.count(str)){
+                result=min(result,solve(j+1,s,st,n));
+            }
+        }
+        return t[i]=result;
+    }
     int minExtraChar(string s, vector<string>& dictionary) {
-        set<char>st;
-        for(auto word:dictionary){
-            for(auto ch:word){
-                st.insert(ch);
-            }
-        }
-        int ans=0;
-        for(auto ch:s){
-            if(st.count(ch)==0){
-                ans++;
-            }
-        }
-        return ans;
+        memset(t,-1,sizeof(t));
+        int n=s.size();
+        set<string>st(begin(dictionary),end(dictionary));
+        return solve(0,s,st,n);
     }
 };
