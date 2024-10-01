537891d88d57bdd4564084c8b983fa9d0e9a5097
Ashab Asir <113583194+Ashab-Asir@users.noreply.github.com>
2024-10-01 14:45:54 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            int rem=(abs(arr[i]))%k;
+            mp[rem]++;
+        }
+        if(mp[0]%2!=0){
+            return false;
+        }
+        for(int i=1;i<=k/2;i++){
+            int remaining =k-i;
+            if(mp[remaining]!=mp[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
