9ef0f5e952e7de6f0dd64a6461256f17ec1cf8b9
Ashab Asir <113583194+Ashab-Asir@users.noreply.github.com>
2024-10-02 07:24:09 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        map<int,vector<int>>mp;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]].push_back(i);
+        }
+        int rank=1;
+        vector<int>ans(arr.size(),0);
+        for(auto element:mp){
+        
+            cout<<element.first<< " ";
+            for(int i=0;i<element.second.size();i++){
+                int ind=element.second[i];
+                ans[i]=rank;
+            }
+            rank++;
+        }
+        return ans;
+    }
+};
