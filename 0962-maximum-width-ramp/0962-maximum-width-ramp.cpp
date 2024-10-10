7f95874ded8fdf8c33c03e449bbba1e982619a56
Ashab Asir <113583194+Ashab-Asir@users.noreply.github.com>
2024-10-10 23:25:21 +0600

    Time: 42 ms (57.39%), Space: 47.2 MB (18.67%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.cpp
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int maxi=0;
     int maxWidthRamp(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            for(int j=nums.size()-1;j>=0 && j>i;j--){
-                if(j-i<=maxi){
-                    break;
-                }
-                if(nums[i]<=nums[j]){
-                    maxi=max(maxi,j-i);
-                    break;
-                }
-            }
+        int n=nums.size();
+        vector<int>temp(n,0);
+        temp[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            temp[i]=max(temp[i+1],nums[i]);
         }
-        return maxi;
+        int i=0,j=0;
+        int ans=0;
+        while(j<n){
+            while(i<j && nums[i]>temp[j]){
+                i++;
+            }
+            ans=max(ans,j-i);
+            j++;
+        }
+        return ans;
     }
 };
