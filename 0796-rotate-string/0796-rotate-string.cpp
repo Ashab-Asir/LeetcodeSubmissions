999c928fc796f3246027c8feef54b3c0b19bb630
Ashab Asir <113583194+Ashab-Asir@users.noreply.github.com>
2024-11-03 09:07:54 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size()){
+            return false;
+        }
+        string tempStr1=s,tempStr2=goal;
+        sort(tempStr1.begin(),tempStr1.end());
+        sort(tempStr2.begin(),tempStr2.end());
+        if(tempStr1!=tempStr2){
+            return false;
+        }
+        reverse(s.begin(), s.end());
+        while (true) {
+            goal.pop_back();
+            s.pop_back();
+            if(s.size()==0){
+                return false;
+            }
+            string temp = s;
+            reverse(s.begin(), s.end());
+            if (s == goal) {
+                return true;
+            }
+            else{
+                s=temp;
+            }
+        }
+        return false;
+    }
+};
