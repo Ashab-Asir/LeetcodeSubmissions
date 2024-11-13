06f99c932707b0e88ed2989ce04c604698152845
Ashab Asir <113583194+Ashab-Asir@users.noreply.github.com>
2024-11-13 23:48:01 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        sort(nums.begin(),nums.end());
+        deque<int>v;
+        for(int i=0;i<nums.size();i++){
+            v.push_back(nums[i]);
+        }
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            v.pop_front();
+            auto low=lower_bound(v.begin(),v.end(),lower-nums[i]);
+            auto high=upper_bound(v.begin(),v.end(),upper-nums[i]);
+            ans+=(high-low);
+        }
+        return ans;
+    }
+};
