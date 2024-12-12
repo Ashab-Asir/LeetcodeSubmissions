3403b69afc5f0c3f5e99ff78c533058eacb25afe
Ashab Asir <113583194+Ashab-Asir@users.noreply.github.com>
2024-12-12 09:27:59 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        int n=gifts.size();
+        while(k--){
+            sort(gifts.begin(),gifts.end());
+            gifts[n-1]=sqrt(gifts[n-1]);
+        }
+        int ans=0;
+        for(int i=0;i<gifts.size();i++){
+            ans+=gifts[i];
+        }
+        return ans;
+    }
+};
