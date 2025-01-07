3c544fa1fd71371cec1d0b0f70e4979302bc3fc0
Ashab Asir <113583194+Ashab-Asir@users.noreply.github.com>
2025-01-07 19:04:16 +0600

    Time: 0 ms (100%), Space: 11.7 MB (33.76%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
@@ -3,16 +3,19 @@ public:
     vector<string> stringMatching(vector<string>& words) {
         int n=words.size();
         vector<string>ans;
+        set<string>st;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i==j){
                     continue;
                 }
-                
                 if(words[j].find(words[i])!=string::npos){
-                    ans.push_back(words[i]);
+                    st.insert(words[i]);
                 }
             }
+        }
+        for(auto x:st){
+            ans.push_back(x);
         }
         return ans;
     }
