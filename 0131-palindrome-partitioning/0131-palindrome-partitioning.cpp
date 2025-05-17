fa9774aa4a919a0fb4c73054a9e1b53e13e913d1
Ashab Asir <113583194+Ashab-Asir@users.noreply.github.com>
2025-05-17 20:02:05 +0600

    Time: 58 ms (46.49%), Space: 61.7 MB (50%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    bool isPalindrom(int start,int end,string s){
-        while(start<end){
-            if(s[start]!=s[end]){
+    bool isSafe(string s,int start,int end){
+        while(start<=end){
+            if(s[start++]!=s[end--]){
                 return false;
-            }
-            else{
-                start++;
-                end--;
             }
         }
         return true;
     }
-    void solve(int ind,string s,vector<string>&path,vector<vector<string>>&ans){
-        if(ind==s.size()){
+    void solve(int index,string s,vector<string>&path,
+    vector<vector<string>>&ans){
+        if(index==s.size()){
             ans.push_back(path);
             return;
         }
-        for(int i=ind;i<s.size();i++){
-            if(isPalindrom(ind,i,s)){
-                path.push_back(s.substr(ind,i-ind+1));
+        for(int i=index;i<s.size();i++){
+            if(isSafe(s,index,i)){
+                path.push_back(s.substr(index,i-index+1));
                 solve(i+1,s,path,ans);
                 path.pop_back();
             }
